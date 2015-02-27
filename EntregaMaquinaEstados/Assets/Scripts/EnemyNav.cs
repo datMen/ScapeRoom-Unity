8186e72a469c111f7950dc2a507952eb312a030d
LouK <8thelouk8@gmail.com>
2015-02-27 16:58:46 +0100

    Added NavMesh system
    One waypoint that changes its position randomly when the enemy is too
    close to it

---
+++ EntregaMaquinaEstados/Assets/Scripts/EnemyNav.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyNav : MonoBehaviour {
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Transform nav_floor;
+
+    private Vector3 nav_floor_size;
+    private NavMeshAgent agent;
+    float taget_max_distance = 2.5F;
+
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        nav_floor_size = nav_floor.transform.localScale*5;
+        setRandomWayPoint();
+    }
+    
+    void Update () {
+        if (getWayPointDistance() < taget_max_distance) { // Agent is too close to the waypoint
+            setRandomWayPoint();
+        }
+    }
+
+    float getWayPointDistance() {
+        return Vector3.Distance(transform.position, target.position);
+    }
+
+    // 
+    void setRandomWayPoint() {
+        target.position = new Vector3(Random.Range(-nav_floor_size.x, nav_floor_size.x), target.position.y, Random.Range(-nav_floor_size.z, nav_floor_size.z));
+        agent.SetDestination(target.position);
+    }
+}
---
+++ EntregaMaquinaEstados/Assets/Scripts/EnemySight.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemySight : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    void OnTriggerEnter(Collider col) {
+    }
+}
